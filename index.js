d8172310a134bda64cc313c8daa715bfc84197b6
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-24 23:13:28 +0000

    added questions in the form of objects within an array to project

--- index.js
+++ index.js
@@ -4,8 +4,89 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
+// create obects within an array for this section
 const questions = [
+    // Title section
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"   
+    },
 
+    // Description section
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project?"   
+    },
+    
+    //Installation section
+    {
+        type: "input",
+        name: "installation",
+        message: "How is the program installed? Please provide imformation on all required softwares and commands"   
+    },
+
+    // Usage section
+    {
+        type: "input",
+        name: "usage",
+        message: "How is the program used?"   
+    },
+
+    // License section
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for your project?",
+        options: [
+            "Apache license 2.0",
+            "Boost Software License 1.0",
+            "Creative Commons Attribution 4.0",
+            "European Union Public License 1.1",
+            "GNU General Public License v3.0",
+            "ISC",
+            "LaTeX Project Public License v1.3c",
+            "Microsoft Public License",
+            "MIT",
+            "Mozilla Public License 2.0",
+            "Open Software License 3.0",
+            "PostgreSQL License",
+            "SIL Open Font License 1.1",
+            "University of Illinois/NCSA Open Source License",
+            "The Unlicense",
+            "zLib License",   
+        ],  
+    },
+
+    // Contribution section
+    {
+        type: "input",
+        name: "contributing",
+        message: "How can users contribute to your project?"   
+    },
+
+    // Tests section
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide testing instructions for your project."   
+    },
+
+    // Questions section
+    // Github Username
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your Github username. 💻"
+    },
+
+    // Email Address
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address? 📧" 
+    },
 ];
 
 // function to write README file
