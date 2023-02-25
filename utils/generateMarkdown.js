d1eac815afa41d406f977e7bc4004facf6ca43ae
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-02-25 01:38:25 +0000

    added function to generate license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,16 @@
+// function to render license badge
+function renderLicensebadge (license) {
+  if(license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return ''
+}
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicensebadge(data.license)}
 `;
 }
 
